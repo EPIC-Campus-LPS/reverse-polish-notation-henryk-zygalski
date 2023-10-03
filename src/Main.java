6d904b0a41ab3a278c6ce2bbd29dea2809227086
cardinn454_lpsk12 <Cardinn454@lpsk12.org>
2023-10-03 09:29:04 -0600

    refactor all code

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(PIP.infixToPostfix("a+b*c+(d*e+f)*g"));
+        System.out.println(PIP.infixToPostfix("a+b*c+d/e+f*g"));
     }
 }
--- src/PIP.java
+++ src/PIP.java
@@ -27,113 +27,60 @@ public class PIP {
 
                 output = output + read;
 
-                //reads whether it is a + or a -
-            } else if (read == '+' || read == '-') {
+            }
 
-                //checks if the stack is empty
-                if (stack.size() == 0) {
-                    System.out.println("added" + read);
+            //equivalent to running until finds an operator
+            else if (read != '(') {
+
+                //pushes whatever read if stack is empty
+                if (stack.isEmpty()) {
+
                     stack.push(read);
 
-                    //checks if the top of the stack is a + or -
-                } else if (stack.peek() == '+' || stack.peek() == '-') {
-
-                    output = output + stack.pop();
-                    stack.push(read);
-
-                    //pushes what's read if its not a + or -
-                } else {
-                    stack.push(read);
                 }
 
-                //reads whether it is a * or /
-            } else if (read == '*' || read == '/') {
+                //runs when the operator is * or /
+                else if (read == '*' || read == '/') {
 
-                //checks if the stack is empty
-                if (stack.size() == 0) {
-                    System.out.println("added" + read);
-                    stack.push(read);
+                    //checks if the top of the stack is * or /
+                    if (stack.peek() == '*' || stack.peek() == '/') {
 
-                    //checks if the top of the stack is a + or -
-                } else if (stack.peek() == '+' || stack.peek() == '-') {
-
-                    System.out.println("added" + read);
-                    stack.push(read);
-
-                    //pops the current stack peek and adds what's read
-                } else {
-                    output = output + stack.pop();
-                    stack.push(read);
-                }
-
-            } else if (read == '(') {
-
-                stack.push(read);
-
-                while (read != ')') {
-                    if (Character.isLetter(read)) {
-
-                        output = output + read;
-
-                        //reads whether it is a + or a -
-                    } else if (read == '+' || read == '-') {
-
-                        //checks if the stack is empty
-                        if (stack.size() == 0) {
-                            System.out.println("added" + read);
-                            stack.push(read);
-
-                            //checks if the top of the stack is a + or -
-                        } else if (stack.peek() == '+' || stack.peek() == '-') {
-
-                            output = output + stack.pop();
-                            stack.push(read);
-
-                            //pushes what's read if its not a + or -
-                        } else {
-                            stack.push(read);
-                        }
-
-                        //reads whether it is a * or /
-                    } else if (read == '*' || read == '/') {
-
-                        //checks if the stack is empty
-                        if (stack.size() == 0) {
-                            System.out.println("added" + read);
-                            stack.push(read);
-
-                            //checks if the top of the stack is a + or -
-                        } else if (stack.peek() == '+' || stack.peek() == '-') {
-
-                            System.out.println("added" + read);
-                            stack.push(read);
-
-                            //pops the current stack peek and adds whats read
-                        } else {
-                            output = output + stack.pop();
-                            stack.push(read);
-                        }
-                    }
-                }
-
-                for (int c = 0; c > stack.size(); i++) {
-                    if (read == ')') {
-                        stack.pop();
-                    } else {
                         output = output + stack.pop();
+                        stack.push(read);
 
                     }
+                    //runs if top of stack is + or -
+                    else {
+
+                        stack.push(read);
+
+                    }
+
                 }
-            }
-            //puts everything left in the stack into the output
-            for (int s = 0; s <= stack.size(); s++) {
-                if (stack.size() == 0) {
-                    break;
+
+                //runs when the operator is + or -
+                else {
+
+
+
                 }
-                System.out.println("popped" + stack.peek());
-                output = output + stack.pop();
+
             }
+            //runs only when an open parenthesis is found
+            else {
+
+                continue;
+
+            }
+
         }
+
+        for (int s = 0; s <= stack.size(); s++){
+
+            output = output + stack.pop();
+
+        }
+
         //returns the output
         return output;
     }
