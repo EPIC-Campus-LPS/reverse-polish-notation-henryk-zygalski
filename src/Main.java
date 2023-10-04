949afd7ad8006683d5e39931063e8a9c0dd96e9b
cardinn454_lpsk12 <Cardinn454@lpsk12.org>
2023-10-04 11:58:29 -0600

    finished infixtopostfix and added some of the second method

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println(PIP.infixToPostfix("a+b*c+d*e+f*g"));
+        System.out.println(PIP.evaluatePostfix("74+"));
+        System.out.println(PIP.infixToPostfix("a + b * c + ( d * e + f ) * g"));
     }
 }
--- src/PIP.java
+++ src/PIP.java
@@ -1,123 +1,136 @@
 import java.util.Stack;
 
-
 public class PIP {
-
-
-//    static String evaluatePostfix (String input){
-
-
-//    }
-
-
-
-
-    static String infixToPostfix(String input) {
-
+    static String evaluatePostfix(String input) {
 
         //extracts the input as a string
         String string = input;
 
+        //defines all of the variables & stack
+        int output = string.charAt(0);
+        Stack<Integer> stack = new Stack<>();
 
-        //defines the output & stack
-        String output = "";
-        Stack<Character> stack = new Stack<>();
-        //repeats from the string length
         for (int i = 0; i < string.length(); i++) {
-
 
             //reads every character of the string
             char read = string.charAt(i);
 
+            //adds every variable/digit to stack
+            if (Character.isLetterOrDigit(read)) {
+
+                stack.push(Integer.valueOf(read));
+
+            } else {
+
+                int answer = stack.pop() + stack.pop();
+                stack.push(answer);
+                Syst
+
+            }
+        }
+
+        output = output + stack.pop();
+
+        return String.valueOf(output);
+    }
+
+    static String infixToPostfix(String input) {
+
+        //extracts the input as a string
+        String string = input;
+
+        //defines the output & stack
+        String output = "";
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < string.length(); i++) {
+
+            //reads every character of the string
+            char read = string.charAt(i);
 
             //adds every letter to the output as it goes along
-            if (Character.isLetter(read)) {
-
+            if (Character.isLetterOrDigit(read)) {
 
                 output = output + read;
 
-
             }
 
-
             //equivalent to running until finds an operator
-            else if (read != ')') {
-
+            else if (read != ')' && read != ' ') {
 
                 //pushes whatever read if stack is empty
                 if (stack.isEmpty() || read == '(') {
 
-
                     stack.push(read);
 
-
                 }
-
 
                 //runs when the operator is * or /
                 else if (read == '*' || read == '/') {
 
-
-                    //checks if the top of the stack is or /
+                    //checks if the top of the stack is * or /
                     if (stack.peek() == '*' || stack.peek() == '/') {
+
                         output = output + stack.pop();
                         stack.push(read);
+
                     }
                     //runs if top of stack is + or -
                     else {
 
-
                         stack.push(read);
-
 
                     }
 
-
                 }
 
-
-                //runs when the operator is + or - but also not a parenthesis
+                //runs when the operator is + - or (
                 else {
 
-
-                    while(!stack.isEmpty() && stack.peek() != '(') {
-
+                    while (!stack.isEmpty() && stack.peek() != '(') {
 
                         output = output + stack.pop();
 
-
                     }
 
-
                     stack.push(read);
-
-
                 }
-
 
             }
             //runs only when a closed parenthesis is found
-            else {
-                //empties stack when stack isn't empty or open parenthesis isn't found
+            else if (read != ' ') {
+
                 while (!stack.isEmpty() && stack.peek() != '(') {
+
                     output = output + stack.pop();
+
+
                 }
-                //removes open parenthesis from stack
+
+                //once the open parenthesis is found it pops it
                 if (stack.peek() == '(') {
                     stack.pop();
                 }
+
             }
-        }
-        //empties stack
-        while (!stack.isEmpty()) {
-            output = output + stack.pop();
+
         }
 
+        //empties out the stack when there is nothing to read
+        while (!stack.isEmpty()) {
+
+            //pops out the open parenthesis
+            if (stack.peek() == '(') {
+
+                stack.pop();
+
+            }
+
+            output = output + stack.pop();
+
+        }
 
         //returns the output
         return output;
     }
 }
-
-
-
